88aaf78621c45bdbab622ded57df4286358a5d0d
OlgaMunaieva <asyssync@gmail.com>
2023-04-26 09:32:54 +0300

    traing

---
+++ Screens/RegistrationScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const RegistrationScreen = () => {
+  return (
+    <View>
+      <Text>Hi, friend!</Text>
+    </View>
+  );
+};
+
+export default RegistrationScreen;
