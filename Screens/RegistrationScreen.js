222abe33bac402a68d9263e9167b3df8c8ddf4a9
OlgaMunaieva <asyssync@gmail.com>
2023-04-27 14:36:48 +0300

    submit form

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
 import {
-  Button,
   ImageBackground,
   KeyboardAvoidingView,
   Platform,
@@ -11,49 +11,88 @@ import {
   View,
 } from "react-native";
 
+const initialUser = {
+  login: "",
+  email: "",
+  password: "",
+};
+
 const RegistrationScreen = () => {
   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
-  return (
-    <ImageBackground
-      source={require("../assets/images/bg.jpg")}
-      resizeMode="cover"
-      style={styles.image}
-    >
-      <View style={styles.form}>
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-        >
-          <Text style={styles.text}>Регистрация</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Логин"
-            onFocus={() => setIsShowKeyboard(true)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Адрес электронной почты"
-            onFocus={() => setIsShowKeyboard(true)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Пароль"
-            secureTextEntry={true}
-            onFocus={() => setIsShowKeyboard(true)}
-          />
-        </KeyboardAvoidingView>
+  const [user, setUser] = useState(initialUser);
 
-        <View
-          style={{
-            ...styles.buttonContainer,
-            marginBottom: isShowKeyboard ? -60 : 0,
-          }}
-        >
-          <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-            <Text style={styles.btnTitle}>Зарегистрироваться</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </ImageBackground>
+  const hideKeyboard = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    // console.log(user);
+  };
+
+  const submitForm = () => {
+    console.log(user);
+    setUser(initialUser);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={() => hideKeyboard()}>
+      <ImageBackground
+        source={require("../assets/images/bg.jpg")}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <TouchableWithoutFeedback onPress={() => hideKeyboard()}>
+          <View style={styles.form}>
+            <KeyboardAvoidingView
+              behavior={Platform.OS === "ios" ? "padding" : "height"}
+            >
+              <Text style={styles.text}>Регистрация</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Логин"
+                value={user.login}
+                onFocus={() => setIsShowKeyboard(true)}
+                onChangeText={(value) =>
+                  setUser((prevState) => ({ ...prevState, login: value }))
+                }
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Адрес электронной почты"
+                value={user.email}
+                onFocus={() => setIsShowKeyboard(true)}
+                onChangeText={(value) =>
+                  setUser((prevState) => ({ ...prevState, email: value }))
+                }
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Пароль"
+                secureTextEntry={true}
+                value={user.password}
+                onFocus={() => setIsShowKeyboard(true)}
+                onChangeText={(value) =>
+                  setUser((prevState) => ({ ...prevState, password: value }))
+                }
+              />
+            </KeyboardAvoidingView>
+
+            <View
+              style={{
+                ...styles.buttonContainer,
+                marginBottom: isShowKeyboard ? -60 : 0,
+              }}
+            >
+              <TouchableOpacity
+                style={styles.button}
+                activeOpacity={0.7}
+                onPress={() => submitForm()}
+              >
+                <Text style={styles.btnTitle}>Зарегистрироваться</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </ImageBackground>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -70,6 +109,7 @@ const styles = StyleSheet.create({
     }),
   },
   form: {
+    paddingTop: 92,
     backgroundColor: "#FFFFFF",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
@@ -92,7 +132,7 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     marginHorizontal: 16,
-    marginBottom: -60,
+    // marginBottom: -60,
     // height: 51,
     // borderRadius: 100,
   },
